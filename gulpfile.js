b0c277dcc9c5e082c8bf3089a12424227fb63019
ILLUSIAsveta <59977609+ILLUSIAsveta@users.noreply.github.com>
2020-03-03 15:05:31 +0300

    Added a task for Gulp that minimizes CSS files and adds an ending to them .min.css.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
+//Подключаем модуль галпа
 const gulp = require('gulp');
+// Подкдючаем модуль mini-css
+const min = require('gulp-mini-css');
+const minCSS =require('gulp-clean-css');
+
 const  browserSync = require('browser-sync').create();
+
 
 gulp.task ('hello', function(done){
     console.log ('Привет мир!');
@@ -15,3 +21,12 @@ gulp.task('browser-sync', function() {
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+gulp.task('min', function(){
+    return gulp.src('src/css/*.css')
+    .pipe(minCSS())
+    .pipe(min({ext:'-min.css'}))
+    .pipe(gulp.dest('dist/css'));
+});
+
+
