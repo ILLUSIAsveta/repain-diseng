a8dcbd68cab9a59a3ba8ae8b127edb361082eb4e
ILLUSIAsveta <59977609+ILLUSIAsveta@users.noreply.github.com>
2020-04-08 21:44:26 +0300

    feat: add projects.sass and add carousel sliders

--- src/js/main.js
+++ src/js/main.js
@@ -55,12 +55,6 @@ $(document).ready(function (){
         });
         
         
-
-
-
-
-
-
 $('body').append('<button class="btn_up"/>');
 
 $('.btn_up').click(function(){
@@ -79,4 +73,30 @@ $('window').scroll(function(){
   }
 });
 
-}); 
+
+
+  //initialize swiper when document ready
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+   
+  })
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() +10 +bullets.width()+10)
+  bullets.css('left', prev.width() +10 )
+  }); 
+
+
+
+
