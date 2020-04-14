af3a8a2e237534534615cca6b2d55219103644cb
ILLUSIAsveta <59977609+ILLUSIAsveta@users.noreply.github.com>
2020-04-14 17:07:33 +0300

    add map

--- src/js/main.js
+++ src/js/main.js
@@ -224,6 +224,45 @@ $('window').scroll(function(){
   // Маска для ввода телефона
 
   $('[type=tel]').mask('+7(000) 00-00-000', {placeholder: "+7 (___) __-__-___"});
+
+// Подключение карты
+
+
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.927263, 30.359172],
+            zoom: 9
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Наш офис',
+            balloonContent: 'Вход со двора'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/placeholder.png',
+            // Размеры метки.
+            iconImageSize: [32, 32],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        });
+
+       
+
+    myMap.geoObjects
+        .add(myPlacemark)
+        
+});
   }); 
 
 
